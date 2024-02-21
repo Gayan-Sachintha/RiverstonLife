ad4bcb3cd4784867c1bc20d7f885accb54db294d
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2024-02-21 19:12:58 +0530

    create the footer of the page

---
+++ View/user/footer.php
@@ -0,0 +1,91 @@
+
+    <footer class="text-center text-lg-start  text-white" style="background-color: #222823;">
+
+      <section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
+
+
+        <div>
+          <a href="" class="me-4 text-reset">
+            <i class="bi bi-facebook"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="bi bi-instagram"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="bi bi-whatsapp"></i>
+          </a>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container text-center text-md-start mt-5  text-white">
+          <div class="row mt-3">
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4">
+                <i class="bi bi-house-door-fill"></i>&nbsp;RIVERSTON LIFE
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                Products
+              </h6>
+              <p>
+                <a href="#!" class="text-reset">VILLA</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">PACKAGES</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Vue</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                NAVIGATION LINKS
+              </h6>
+              <p>
+                <a href="#!" class="text-reset">HOME</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">ABOUT US</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">CONTACT US</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                info@example.com
+              </p>
+              <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2024 Copyright ULTIMATE 5 TEAM
+      </div>
+      <!-- Copyright -->
+    </footer>
