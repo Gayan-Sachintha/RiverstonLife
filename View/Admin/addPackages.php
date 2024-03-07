dd0493375bc5305eacbcefe52ac7473d1fa5dc11
Dimantha Thihara Walallawita <87009586+DimanthaWalallawita@users.noreply.github.com>
2024-03-07 13:43:07 +0530

    init commit

--- View/Admin/addPackages.php
+++ View/Admin/addPackages.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Add Instructors</title>
+    <title>Add Packages</title>
 
     <!-- Custom fonts for this template-->
     <link href="assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
