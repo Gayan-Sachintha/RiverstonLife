1ece983e70b21fdea9aea573134502abb57c1818
Dimantha Thihara Walallawita <87009586+DimanthaWalallawita@users.noreply.github.com>
2024-03-22 01:20:25 +0530

    init commit

--- Admin/header.php
+++ Admin/header.php
@@ -70,8 +70,6 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Utilities:</h6>
                         <a class="collapse-item" href="viewCalender.php">View Calendar</a>
-                        <a class="collapse-item" href="#">Update Calendar</a>
-                        <a class="collapse-item" href="#">Cancel Event</a>
                     </div>
                 </div>
             </li>
